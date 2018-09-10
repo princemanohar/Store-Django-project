a994821da6a6758d3fd3a21b658404d5ec1cb4e7
Prince Manohar <princemanohar0909@gmail.com>
2018-09-10 21:49:11 +0530

    push myapp file

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import patterns, include, url
 from . import views
 from myapp.views import StoresListView
 from myapp.views import StoreView
+from myapp.views import ReviewView
 urlpatterns = [
     url(r'^storesList', StoresListView.as_view(), name='store-list'),
     url(r'^stores', StoreView.as_view(), name='store'),
+    url(r'^reviews', ReviewView.as_view(), name='review'),
     url('', views.stores_list, name='stores_list'),
 
 
--- myapp/views.py
+++ myapp/views.py
@@ -29,7 +29,9 @@ def stores_list(request):
     return JsonResponse(final_data, safe=False)
     #return render(request, 'store.html', {})
 
-
+''''
+This class contains the logic to handle date for the Store model.
+'''
 
 class StoreView(View):
     def get(self, request, *args, **kwargs):
@@ -74,5 +76,36 @@ class StoresListView(View):
 
         return JsonResponse(final_data, safe=False)
 
+''''
+This class will contains the logic to handle date of Reviews model.
+'''
 
+class ReviewView(View):
+    def get(self, request, *args, **kwargs):
+        allReviews = Reviews.objects.all()
+        data = serializers.serialize('json', allReviews)
+        data = json.loads(data)
 
+        final_data = []
+        for data1 in data:
+            final_data.append(data1['fields'])
+
+        return JsonResponse(final_data, safe=False)
+
+    '''
+        Create a Rewvies entry
+    '''
+    def post(self, request, *args, **kwargs):
+        print("POST Data : %s:" % request.POST)
+        print("Body : %s:" % request.body)
+        # Converting body to json object
+        body = json.loads(request.body)
+
+        acs = body.get('store_id')
+        print("AcsMonkey : "  + str(acs))
+        storeobj = Store.objects.get(store_id = acs)
+
+        createResp = Reviews.objects.create( store_id = storeobj , review = body.get('review') , stars = body.get('stars') )
+        print("createResp: %d " % createResp.review_id)
+
+        return JsonResponse(createResp.review_id, safe=False)
